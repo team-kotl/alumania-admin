8c00a89b72518205db784432419be732fa21a3bc
cayeelii <carielmagajoyce@gmail.com>
2024-11-30 19:51:00 +0800

    Fixed heading and added functionality for cancel button

--- admin/src/js/settings.js
+++ admin/src/js/settings.js
@@ -1,43 +1,51 @@
+const logoutAnimation = document.getElementById("logout-animation");
+const generateAnimation = document.getElementById("generate-animation");
+const overlay = document.querySelector('.overlay');
+const cancelBtn = document.querySelector('.cancel-btn');
 
-  //SA ANIMATED ICONS
-   const logoutAnimation = document.getElementById("logout-animation");
-   const generateAnimation = document.getElementById("generate-animation");
+document.querySelector(".logout").addEventListener("mouseenter", () => {
+    logoutAnimation.setAttribute("loop", "true");
+    logoutAnimation.play();
+});
 
-   document.querySelector(".logout").addEventListener("mouseenter", () => {
-       logoutAnimation.setAttribute("loop", "true");
-       logoutAnimation.play();
-   });
+document.querySelector(".logout").addEventListener("mouseleave", () => {
+    logoutAnimation.removeAttribute("loop");
+});
 
-   document.querySelector(".logout").addEventListener("mouseleave", () => {
-       logoutAnimation.removeAttribute("loop");
-   });
+document.querySelector(".generate").addEventListener("mouseenter", () => {
+    generateAnimation.setAttribute("loop", "true");
+    generateAnimation.play();
+});
 
-   document.querySelector(".generate").addEventListener("mouseenter", () => {
-       generateAnimation.setAttribute("loop", "true");
-       generateAnimation.play();
-   });
+document.querySelector(".generate").addEventListener("mouseleave", () => {
+    generateAnimation.removeAttribute("loop");
+    generateAnimation.stop();
+});
 
-   document.querySelector(".generate").addEventListener("mouseleave", () => {
-       generateAnimation.removeAttribute("loop");
-       generateAnimation.stop();
-   });
 
-   //POPUP
-   document.querySelector(".logout").addEventListener("click", () => togglePopup("popup-logout"));
-   document.querySelector(".generate").addEventListener("click", () => togglePopup("popup-generate"));
-   function togglePopup(popupId) {
-       document.getElementById(popupId).classList.toggle("active");
-   }
+//POPUP
+document.querySelector(".logout").addEventListener("click", () => togglePopup("popup-logout"));
+document.querySelector(".generate").addEventListener("click", () => togglePopup("popup-generate"));
+function togglePopup(popupId) {
+    document.getElementById(popupId).classList.toggle("active");
+}
 
-    //COPY NG GENERATE
-   let copyText = document.querySelector(".copy-text");
-   copyText.querySelector("button").addEventListener("click",function(){
-       let input = copyText.querySelector("input.text");
-       input.select();
-       document.execCommand("copy");
-       copyText.classList.add("active");
-       window.getSelection().removeAllRanges();
-       setTimeout(function(){
-           copyText.classList.remove("active");
-       },2500);
-   })
+
+// COPY GENERATED ADMIN KEY
+let copyText = document.querySelector(".copy-text");
+copyText.querySelector("button").addEventListener("click",function(){
+    let input = copyText.querySelector("input.text");
+    input.select();
+    document.execCommand("copy");
+    copyText.classList.add("active");
+    window.getSelection().removeAllRanges();
+    setTimeout(function(){
+        copyText.classList.remove("active");
+    },2500);
+})
+
+// Cancel button
+cancelBtn.addEventListener('click', () => {
+    togglePopup('popup-logout');
+  });
+
