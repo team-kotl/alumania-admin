ae88ea2b47715c8e4bb319d4511e94b709b33fdf
nikkochoy <choynikko@gmail.com>
2024-12-10 23:41:34 +0800

    added generate key function in settings

---
+++ admin/src/core/generateKey.php
@@ -0,0 +1,20 @@
+<?php
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $length = isset($_GET['length']) ? intval($_GET['length']) : 10;  // Default length is 10
+    echo generateRandomString($length);
+}
+
+?>
