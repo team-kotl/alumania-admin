f3a34702c4c7f8cddeae897856adf5635f389514
yukiroow <hcdominguez07@gmail.com>
2025-01-29 23:44:34 +0800

    Add JWT session handling in auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
 const db = require("../db").db;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_TOKEN = process.env.JWT_TOKEN;
+const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
 
 router.post("/login", (req, res) => {
     const { username, password } = req.body;
     console.log(`[LOGIN REQUEST] Username: ${username}, IP: ${req.ip}`);
 
     db.query(
-        `SELECT username FROM user WHERE username = ? AND password = ? AND usertype = ?`,
-        [username, password, 'admin'],
+        `SELECT username, usertype FROM user WHERE username = ? AND password = ?`,
+        [username, password],
         (err, results) => {
             if (err) {
                 console.log(
@@ -16,12 +21,52 @@ router.post("/login", (req, res) => {
                 return res.status(500).json({ error: "Internal Server Error" });
             }
             if (results.length < 1) {
-                res.status(401).json({ error: "Invalid Credentials" });
+                res.status(403).json({ error: "Invalid Credentials" });
             } else {
-                res.status(200).json({ message: "Successful login" });
+                const token = jwt.sign(
+                    { user: results[0].username },
+                    JWT_TOKEN,
+                    { expiresIn: JWT_EXPIRATION }
+                );
+                res.status(200).json({ 
+                    username: results[0].username,
+                    usertype: results[0].usertype,
+                    token: token
+                });
             }
         }
     );
+});
+
+// Token refresh endpoint
+router.post("/refresh-token", (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+
+    jwt.verify(token, JWT_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: "Invalid or expired token" });
+        }
+
+        const newToken = jwt.sign({ user: decoded.user }, JWT_TOKEN, {
+            expiresIn: JWT_EXPIRATION,
+        });
+        res.status(200).json({ token: newToken });
+    });
+});
+
+// Validate JSON web token of user
+router.get("/protected", (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ error: "No token provided" });
+
+    jwt.verify(token, JWT_TOKEN, (err, decoded) => {
+        if (err) return res.status(401).json({ error: "Invalid token" });
+        res.json({ message: `goods` });
+    });
 });
 
 module.exports = router;
