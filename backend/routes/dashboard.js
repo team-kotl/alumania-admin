c6840b2dbaf82e872efb3a291f8da5492aeace5e
blueskatchy <cjaelapig@gmail.com>
2025-02-27 23:40:42 +0800

    Add dashboard router

---
+++ backend/routes/dashboard.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const db = require("../db").db;
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const authRouter = require("./routes/auth");
+const dashboardRouter = require("./routes/dashboard");
 
 app.use("/auth", authRouter);
+app.use("/dashboard", dashboardRouter);
+
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
